466e5d2f52e64db21d90211363a6b0e714ab11cb
hargitayjpl <hargitay@jpl.nasa.gov>
2024-03-03 13:08:11 -0800

    Add destroying of AWS Resources

--- nightly_tests/destroy.sh
+++ nightly_tests/destroy.sh
@@ -54,11 +54,16 @@ export STACK_NAME="unity-management-console-${PROJECT_NAME}-${VENUE_NAME}"
 echo "Initiating Cloudformation Teardown..." >> nightly_output.txt
 echo "Initiating Cloudformation Teardown..."
 
+# Start Selenium docker iamge
+DOCKER_ID=$(sudo docker run -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome)
+sleep 10
 # Uninstall AWS resources through MC
 python3 uninstall_aws_resources_mc.py
 
+# Stop Selenium docker iamge
+echo "Stopping Selenium docker container..."
+sudo docker stop $DOCKER_ID
 # Wait for some time for MC AWS Resources to unistall
-sleep 360
 
 aws cloudformation delete-stack --stack-name ${STACK_NAME}
 
