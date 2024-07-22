7f6e9d6a5fd2b6e96a8fcfb207b8f2270cccb12f
mike-gangl <59702631+mike-gangl@users.noreply.github.com>
2024-07-22 17:47:18 -0400

    Health schema (#12)
    * added health services schema
    
    * dded health schema check in unity-test
    
    * updated lock file
    
    * added checks for health service updates to kick off unity-py testing

--- libs/unity-py/tests/test_unity_health_service.py
+++ libs/unity-py/tests/test_unity_health_service.py
@@ -7,6 +7,8 @@ import pytest
 
 from unity_sds_client.unity import Unity
 from unity_sds_client.unity_services import UnityServices
+import json
+from jsonschema import validate
 
 
 @pytest.mark.regression
@@ -26,6 +28,12 @@ def test_health_status_retrieval():
     s = Unity()
     health_service = s.client(UnityServices.HEALTH_SERVICE)
     health_statuses = health_service.get_health_status()
+
+    f = open('../../schemas/health-service/health-services.schema.json')
+    schema = json.load(f)
+
+    validate(instance=health_statuses, schema=schema)
+
     assert health_statuses is not None
 
 @pytest.mark.regression
