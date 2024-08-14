6f322d35a76f06960d4d906952c4c8bb90790e1e
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2024-08-14 14:38:00 -0700

    Fixed issues with previous checks

--- tests/system-tests/features/steps/apgs/steps.py
+++ tests/system-tests/features/steps/apgs/steps.py
@@ -1,15 +1,15 @@
 import os
 import time
 
-from behave import step_registry
+from behave import *  # noqa: F403
 from unity_sds_apgs_client.models import AdsAcbMcpCloneGet200Response
 from unity_sds_client.services.application_service import DockstoreAppCatalog
 from unity_sds_client.unity import Unity, UnityEnvironments
 from unity_sds_client.unity_services import UnityServices as services
 
 
-@step_registry.given("I have a token to authenticate with Unity Services")
-def step_impl(context):  # noqa: F811
+@given("I have a token to authenticate with Unity Services")  # noqa: F405
+def step_impl(context):
     if "develop" in context.tags:
         s = Unity(UnityEnvironments.DEV)
     elif "test" in context.tags:
@@ -28,14 +28,10 @@ def step_impl(context):  # noqa: F811
         raise Exception("Unable to get a token from the unity environment")
 
 
-@step_registry.when("I submit {repo_name} to the apgs build system")
+@when("I submit {repo_name} to the apgs build system")  # noqa: F405
 def step_impl(context, repo_name):  # noqa: F811
     # Find existing version of the package on dockstore:
-    dockstore_api = os.environ.get("DOCKSTORE_API", None)
-    dockstore_token = os.environ.get("DOCKSTORE_TOKEN", None)
-    if dockstore_token is None or dockstore_api is None:
-        raise Exception("Dockstore API and/or Credentials are not set.")
-    app_catalog = DockstoreAppCatalog(dockstore_api, dockstore_token)
+    app_catalog = get_dockstore()
     dockstore_app = os.path.basename(repo_name)
     print("Searching for {}".format(dockstore_app))
     for app in app_catalog.application_list(published=True):
@@ -54,21 +50,16 @@ def step_impl(context, repo_name):  # noqa: F811
     context.response = response
 
 
-@step_registry.then("the apgs build response is success")
+@then("the apgs build response is success")  # noqa: F405
 def step_impl(context):  # noqa: F811
     response = context.response
     assert isinstance(response, AdsAcbMcpCloneGet200Response)
     assert response.clone_url == context.repo_name
 
 
-@step_registry.then("I wait for the apgs build to complete")
+@then("I wait for the apgs build to complete")  # noqa: F405
 def step_impl(context):  # noqa: F811
-    dockstore_api = os.environ.get("DOCKSTORE_API", None)
-    dockstore_token = os.environ.get("DOCKSTORE_TOKEN", None)
-    if dockstore_token is None or dockstore_api is None:
-        raise Exception("Dockstore API and/or Credentials are not set.")
-    app_catalog = DockstoreAppCatalog(dockstore_api, dockstore_token)
-
+    app_catalog = get_dockstore()
     defaultVersion = context.app_version
 
     check = 0
@@ -84,3 +75,12 @@ def step_impl(context):  # noqa: F811
 
     if defaultVersion == context.app_version:
         raise Exception("defaultVersion was the same after 10 minutes. Failing test")
+
+
+def get_dockstore():
+    dockstore_api = os.environ.get("DOCKSTORE_API", None)
+    dockstore_token = os.environ.get("DOCKSTORE_TOKEN", None)
+    if dockstore_token is None or dockstore_api is None:
+        raise Exception("Dockstore API and/or Credentials are not set.")
+    app_catalog = DockstoreAppCatalog(dockstore_api, dockstore_token)
+    return app_catalog
