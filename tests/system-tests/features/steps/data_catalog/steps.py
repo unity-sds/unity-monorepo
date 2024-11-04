89530941651a5355e895e7ea05426ae202d1af7f
jimmie <jimmie.d.young@gmail.com>
2024-11-04 15:46:44 -0800

    Initial implementation of data catalog test case. Work TBD - see TODO's

--- tests/system-tests/features/steps/data_catalog/steps.py
+++ tests/system-tests/features/steps/data_catalog/steps.py
@@ -1,137 +1,39 @@
-from behave import *
-from behave.runner import Context
+import os
 
-@given('a product exists in an S3 bucket')
-def step_impl(context:Context):
-    raise NotImplementedError(u'a product exists in an S3 bucket')
+from behave import *  # noqa: F403
+from environment import get_value
 
-@given(u'And a collection has been created for this product')
+from unity_sds_client.resources.collection import Collection
+from unity_sds_client.resources.dataset import Dataset
+from unity_sds_client.resources.data_file import DataFile
+
+from unity_sds_client.unity import Unity
+from unity_sds_client.unity import UnityEnvironments
+from unity_sds_client.unity_session import UnitySession
+from unity_sds_client.unity_services import UnityServices as services
+
+from features.steps.import_steps import *
+
+# TODO: Change the call such that a STAC document is returned.
+@when("a collection lookup request is made to the DAPA endpoint")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a collection has been created for this filetype')
+    s = context.unity_session
+    dataManager = s.client(services.DATA_SERVICE)
 
+    collection_id = get_value(context, 'COLLECTION_ID', mandatory=True)
+    print(f"Collection to manipulate is {collection_id}")
+    context.collection_data = dataManager.get_collection_data(Collection(collection_id), limit=100, filter="updated >= '2024-03-18T00:00:00Z' and updated <= '2024-03-21T23:59:59Z'")
 
-@given(u'a CNM has been created for the product')
+# TODO: Change this to validity of the returned STAC document.
+@then("one and only one collection is returned")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a CNM has been created for the product')
+    print(f'I have received {len(context.collection_data)} collections.')
+    for dataset in context.collection_data:
+        print(f'dataset begin time: {dataset.data_begin_time}')
+        print(f'dataset end time: {dataset.data_end_time}')
+        print(f'dataset id: {dataset.id}' )
+    if (len(context.collection_data) < 1):
+        raise Exception("No result collection has been returned.")
+    if (len(context.collection_data) > 1):
+        raise Exception("More than one result collection has been returned.")
 
-
-@when(u'a CNM request is sent to the ingest endpoint')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When a CNM request is sent to the ingest endpoint')
-
-
-@then(u'the product should be searchable in the catalog')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the product should be searchable in the catalog')
-
-
-@then(u'the file maps back to the original file')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the file maps back to the original file')
-
-
-@given(u'an invalid CNM has been created for the product')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given an invalid CNM has been created for the product')
-
-
-@then(u'the product should not be searchable in the catalog')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the product should not be searchable in the catalog')
-
-
-@then(u'a error message has been added to the queue')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then a error message has been added to the queue')
-
-
-@given(u'the CNM message references a non-existent object')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given the CNM message references a non-existent object')
-
-
-@given(u'a collection has not been created for this filetype')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a collection has not been created for this filetype')
-
-
-@then(u'the file size matches the CNM file size')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the file size matches the CNM file size')
-
-# new additions#
-
-@given(u'SounderSIPS data exists at an accessible location')
-def step_impl(context):
-    # copy data from artifact to s3://asdnadjad
-    raise NotImplementedError(u'STEP: Given SounderSIPS data exists at an accessible location')
-
-
-@given(u'the data are not in a Unity owned bucket')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given the data are not in a Unity owned bucket')
-
-
-@given(u'a collection has been created for this product')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a collection has been created for this product')
-
-
-@then(u'the product shall be copied to a Unity owned bucket')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the product shall be copied to a Unity owned bucket')
-
-
-@then(u'the search result maps to the new file')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the search result maps to the new file')
-
-
-@given(u'a registered user')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a registered user')
-
-
-@given(u'a data product to upload')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a data product to upload')
-
-
-@when(u'a user uploads the product')
-def step_impl(context):
-    # os.sys_call(generate_downlaod_list... asdadad)
-    raise NotImplementedError(u'STEP: When a user uploads the product')
-
-
-@then(u'the search result maps to a collection')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the search result maps to a collection')
-
-@given(u'data products are in a Unity owned bucket')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given data products are in a Unity owned bucket')
-
-
-@given(u'a metadata sidecar file in a Unity owned bucket')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a metadata sidecar file in a Unity owned bucket')
-
-
-@when(u'a CNM populated with required files is sent to the Cumulus SNS Topic')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When a CNM populated with required files is sent to the Cumulus SNS Topic')
-
-
-@then(u'the data are cataloged')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the data are cataloged')
-
-
-@then(u'the metadata are cataloged')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the metadata are cataloged')
-
-
-@then(u'a success response is sent to the SPS Process')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then a success response is sent to the SPS Process')
