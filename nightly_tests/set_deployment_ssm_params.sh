cfe9d44bc76d5e651def61c840a54649fde14f38
jimmie <jimmie.d.young@gmail.com>
2024-12-06 16:41:30 -0800

    Refactor of set_deployment_ssm_params.sh to extract functions no defined in define_ssm_functions.sh

--- nightly_tests/set_deployment_ssm_params.sh
+++ nightly_tests/set_deployment_ssm_params.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./define_ssm_functions.sh
+
 # 
 PROJECT_NAME=""
 VENUE_NAME=""
@@ -44,68 +46,6 @@ fi
 
 echo "set_deployment_ssm_params.sh :: PROJECT_NAME: ${PROJECT_NAME}"
 echo "set_deployment_ssm_params.sh :: VENUE_NAME: ${VENUE_NAME}"
-
-#
-# Sub-routine to gracefully delete a SSM parameter
-#
-delete_ssm_param() {
-    local key=$1
-    echo "Deleting SSM parameter: ${key} ..."
-    local lookup=$(aws ssm get-parameter --name "$key" 2>&1)
-    if [[ "$(echo "${lookup}" | grep -q "ParameterNotFound" && echo no)" == "no" ]]; then
-        echo "SSM param ${key} not found.  Not attempting a delete."
-    else
-        aws ssm delete-parameter --name "${key}" || echo "ERROR: SSM delete failed for $key"
-    fi
-}
-
-#
-# Sub-routine to create a SSM parameter, and tag it
-#
-create_ssm_param() {
-    local key=$1
-    local value=$2
-    local capability=$3
-    local capVersion=$4
-    local component=$5
-    local name=$6
-echo "Creating SSM parameter : ${key} = ${value} ..."
-    aws ssm put-parameter --name "${key}" --value "${value}" --type String \
-    --tags \
-    "Key=Venue,Value=${VENUE_NAME}" \
-    "Key=ServiceArea,Value=cs" \
-    "Key=Capability,Value=${capability}" \
-    "Key=CapVersion,Value=${capVersion}" \
-    "Key=Component,Value=${component}" \
-    "Key=Name,Value=${name}" \
-    "Key=Proj,Value=${PROJECT_NAME}" \
-    "Key=CreatedBy,Value=cs" \
-    "Key=Env,Value=${VENUE_NAME}" \
-    "Key=Stack,Value=${component}" 2>/dev/null
-    # TODO: Is there a SSM Description field (to add above)?
-    if [ $? -ne 0 ]; then
-        echo "ERROR: SSM create failed for $key"
-    fi
-}
-
-#
-#
-#
-refresh_ssm_param() {
-    local key=$1
-    local value=$2
-    local capability=$3
-    local capVersion=$4
-    local component=$5
-    local name=$6
-    delete_ssm_param "${key}"
-    create_ssm_param "${key}" "${value}" "${capability}" "${capVersion}" "${component}" "${name}"
-}
-
-get_ssm_val() {
-    local key=$1
-    aws ssm get-parameter --name ${key} --with-decryption --query 'Parameter.Value' --output text
-}
 
 #
 # Create SSM:
