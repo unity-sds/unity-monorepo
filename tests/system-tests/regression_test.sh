21fc968d6da0ac24a675fe35c4bb83d83b7b2e19
jimmie <jimmie.d.young@gmail.com>
2024-12-10 15:26:19 -0800

    Place currently implemented behave test.

--- tests/system-tests/regression_test.sh
+++ tests/system-tests/regression_test.sh
@@ -1,7 +1,9 @@
 # Order matters here!
 # Package an Application
-behave features/apgs/packaging.feature -t test --no-capture
+#  behave features/apgs/packaging.feature -t test --no-capture
 # Run the package* in airflow
-behave features/sps/cwl.feature -t test
+# behave features/sps/cwl.feature -t test
 # Run the pacakge via ogc
 # behave features/sps/wpst.feature -t test
+
+behave features/data_catalog/stac.feature -D COLLECTION_ID=${STAC_COLLECTION_ID} -D STAC_SCHEMA_FILE=${STAC_SCHEMA_FILE} -D TESTRAIL=${USE_TESTRAIL}
