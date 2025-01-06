ec3fa75d64fd28716d14c0badc0f597b3a81c803
leebrian <>
2025-01-05 16:50:37 -0800

    Update U-DS test for collection deleteion

--- libs/unity-py/tests/test_unity_data_service.py
+++ libs/unity-py/tests/test_unity_data_service.py
@@ -1,6 +1,7 @@
 from unity_sds_client.unity import Unity
 from unity_sds_client.unity_exception import UnityException
 from unity_sds_client.unity_session import UnitySession
+from unity_sds_client.unity import UnityEnvironments
 from unity_sds_client.resources.collection import Collection
 from unity_sds_client.unity_services import UnityServices as Services
 
@@ -48,3 +49,18 @@ def test_collection_creation(cleanup_update_test):
     s.set_venue("test")
     ds = s.client(Services.DATA_SERVICE)
     ds.create_collection(Collection("urn:nasa:unity:unity:test:my_collection_id"), True)
+
+def test_collection_deletion():
+
+    unity = Unity(UnityEnvironments.TEST)
+    token = unity._session.get_auth().get_token()
+    data_service = unity.client(Services.DATA_SERVICE)
+
+    collections = data_service.get_collections(100)
+
+    granule_id = 'urn:nasa:unity:emit:dev:unity-tutorial___1:summary_table.txt'
+
+    for i in range(len(collections)):
+        if granule_id in collections[i].collection_id:
+            data_service.delete_collection_data(collections[i], granule_id)
+            break
