a6c86ea3dcc194508c6911d89ba17ad74965c3d2
galenatjpl <ghollins@jpl.nasa.gov>
2025-01-15 14:31:03 -0800

    Add support for test directory and other run-time environment variables

--- tests/system-tests/regression_test.sh
+++ tests/system-tests/regression_test.sh
@@ -1,3 +1,4 @@
+
 # Order matters here!
 # Package an Application
 #  behave features/apgs/packaging.feature -t test --no-capture
@@ -6,4 +7,8 @@
 # Run the pacakge via ogc
 # behave features/sps/wpst.feature -t test
 
-behave features/data_catalog/stac.feature -D COLLECTION_ID=${STAC_COLLECTION_ID} -D STAC_SCHEMA_FILE=${STAC_SCHEMA_FILE} -D TESTRAIL=${USE_TESTRAIL}
+if [[ -z $BASE_TEST_DIR} ]]; then
+    BASE_TEST_DIR =`pwd`
+fi
+
+behave ${BASE_TEST_DIR}/features/data_catalog/stac.feature -D COLLECTION_ID=${STAC_COLLECTION_ID} -D STAC_SCHEMA_FILE=${STAC_SCHEMA_FILE} -D TESTRAIL=${USE_TESTRAIL}
