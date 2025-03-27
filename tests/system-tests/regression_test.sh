0c3c7de336bcff8c436d0ab53c4e3b6bd0345396
brianlee731 <132599772+brianlee731@users.noreply.github.com>
2025-03-27 11:21:01 -0700

    Update regression_test.sh
    Update regression_test.sh to include other tests from U-DS, U-SPS, and U-ADS

--- tests/system-tests/regression_test.sh
+++ tests/system-tests/regression_test.sh
@@ -1,9 +1,24 @@
-
+#
 # Order matters here!
+#
+#
+# Test Application Catalog
+#
+# Test Algorithm registration and retreiveal from the catalog
+behave ${BASE_TEST_DIR}/features/application_catalog/catalog.feature
+
+# Test the capability to publish application packages conforming to the published OGC Best Practice
+behave ${BASE_TEST_DIR}/features/application_catalog/OGC_validations.feature
+
 # Package an Application
 #  behave features/apgs/packaging.feature -t test --no-capture
+
+#
+# Test U-SPS
+#
 # Run the package* in airflow
-# behave features/sps/cwl.feature -t test
+behave features/sps/cwl.feature -t test
+
 # Run the pacakge via ogc
 # behave features/sps/wpst.feature -t test
 
@@ -11,4 +26,17 @@ if [[ -z $BASE_TEST_DIR ]]; then
     BASE_TEST_DIR =`pwd`
 fi
 
+#
+# U-DS Tests after Application executes successfully
+#
+# Test the ingestion of data in S3 - this should be taken care of when running the package in Airflow. 
+behave ${BASE_TEST_DIR}/features/data_catalog/ingest_with_required_metadata.feature
+
+# The data catalog shall return search results in STAC (SpatioTemporal Asset Catalogs) format
 behave ${BASE_TEST_DIR}/features/data_catalog/stac.feature -D COLLECTION_ID=${STAC_COLLECTION_ID} -D STAC_SCHEMA_FILE=${STAC_SCHEMA_FILE} -D TESTRAIL=${USE_TESTRAIL}
+
+# Ensure granules can be found by their parent collections
+behave ${BASE_TEST_DIR}/features/data_catalog/parent_collections.feature
+
+# Test DAPA APIs
+behave ${BASE_TEST_DIR}/features/data_catalog/dapa.feature
