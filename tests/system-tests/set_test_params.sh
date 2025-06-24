3a1e742d8debe1c49f335a9b7ae7e9ee35fb06a4
jimmie <jimmie.d.young@gmail.com>
2025-06-24 10:35:18 -0700

    Add fetch of dockstore info for system test.

--- tests/system-tests/set_test_params.sh
+++ tests/system-tests/set_test_params.sh
@@ -54,11 +54,11 @@ export AIRFLOW_ENDPOINT=$(get_ssm_val "$UNITY_AIRFLOW_ENDPOINT_SSM")
 VENUE_BUCKET_SSM="${SSM_PREFIX}/venue-bucket"
 export VENUE_BUCKET=$(get_ssm_val "$VENUE_BUCKET_SSM")
 
-# DOCKSTORE_API_SSM="${SSM_PREFIX}/dockstore/api"
-# export DOCKSTORE_API=$(get_ssm_val "$DOCKSTORE_API_SSM")
+DOCKSTORE_API_SSM="${SSM_PREFIX}/dockstore/api"
+export DOCKSTORE_API=$(get_ssm_val "$DOCKSTORE_API_SSM")
 
-# DOCKSTORE_TOKEN_SSM="${SSM_PREFIX}/dockstore/token"
-# export DOCKSTORE_TOKEN=$(get_ssm_val "$DOCKSTORE_TOKEN_SSM")
+DOCKSTORE_TOKEN_SSM="${SSM_PREFIX}/dockstore/token"
+export DOCKSTORE_TOKEN=$(get_ssm_val "$DOCKSTORE_TOKEN_SSM")
 
 
 # ---------------------------------------
